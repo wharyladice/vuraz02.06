1d2aeb0526b289192b4a637266a4d0c404270971
Admin <Admin@WIN-5I7J1UL4AE7>
2023-06-02 12:26:12 +0300

    Добавьте файлы проекта.

---
+++ vuraz02.06/vuraz02.06.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "математичний вираз" << endl;
+	int x, p;
+	cout << "write ur x: ";
+	cin >> x;
+	p = (3 * pow(x, 3)) - (2 * pow(x, 2)) + (3 * x) - 1;
+	cout << p;
+}
